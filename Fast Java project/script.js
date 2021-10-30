2ea54fdac1aef3292c70049188fc28e61b969813
Tanvir Ahmed <93043879+tanvirahmed-0@users.noreply.github.com>
2021-10-31 02:30:38 +0600

    Add files via upload

---
+++ Fast Java project/script.js
@@ -0,0 +1,53 @@
+alert("Error Page");
+
+document.write("<h1>Tanvir Ahmed</h1><br/>");
+
+
+document.write("<h3>01700044411</h3><br/>");
+document.write("Bangladesh");
+console.log("It's Hacker probleam");
+
+//Fast types of variable 
+var name = "Tanvir Ahmed";
+document.write(name);
+
+//Third types of variable 
+var name,age,number;
+
+// Second types of variable decleare 
+var name;
+var age;
+
+name = "Tanvir Ahmed";
+age = 18; 
+
+document.write(name);
+document.write(age);
+
+//change variable value 
+var num = 20.4;
+
+num = parseFloat(num); //float type data 
+console.log(typeof(num));
+
+//use Number mathod toFixed
+var number = 4.27934;
+console.log(number.toFixed(3));
+console.log(number.toPrecision(2));
+
+// main number mathod 
+
+console.log(Number(20.34));
+// console.log(typeof(Number(20)));
+console.log(Number(false));
+
+num = toString(num);
+// console.log(typeof(num));
+
+var x = 4;
+y=3; 
+z=x+y;
+document.write(z);
+
+//JavaScrip Literals 
+document.getElementById("test").innerHTML = 'Tanvir Ahmed';
